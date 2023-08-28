7ef8eb32bab762095ebca355bbfc71ab2b74694d
Soumyadeep Ghosh <92305008+KillerMax4299@users.noreply.github.com>
2023-08-28 15:33:38 +0530

    Update Routes.js

--- Backend/src/Routes.js
+++ Backend/src/Routes.js
@@ -2,8 +2,19 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Weather api server live");
+router.post("/getWeather",async (req, res) => {
+  const {value} =await req.body
+  // console.log(value)
+
+  try {
+    const { data } =await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${process.env.API_KEY}&units=metric`
+  );
+  res.json(data);
+  } catch (err) {
+    res.json(err);
+  }
+  
 });
 
 module.exports = router;
