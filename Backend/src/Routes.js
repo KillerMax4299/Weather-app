2abb6d02e96814de5d2e8e69c1afe5aba3a4e026
Soumyadeep Ghosh <92305008+KillerMax4299@users.noreply.github.com>
2023-08-28 16:32:26 +0530

    Update Routes.js

--- Backend/src/Routes.js
+++ Backend/src/Routes.js
@@ -2,19 +2,21 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
-router.post("/getWeather",async (req, res) => {
-  const {value} =await req.body
-  // console.log(value)
-
+router.post("/getWeather", async (req, res) => {
+  const { value } = await req.body;
   try {
-    const { data } =await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${process.env.API_KEY}&units=metric`
-  );
-  res.json(data);
-  } catch (err) {
-    res.json(err);
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${process.env.API_KEY}&units=metric`
+    );
+
+    res.json(data);
+    // res.json(response);
+  } catch {
+    res.status(404).json({
+      cod: "404",
+      message: "city not found",
+    });
   }
-  
 });
 
 module.exports = router;
