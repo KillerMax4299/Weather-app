a02aa5f6ad6c29ca2bcbd93ec83ac9e4a4e20014
Soumyadeep Ghosh <92305008+KillerMax4299@users.noreply.github.com>
2023-09-02 20:11:32 +0530

    1.1.4
    Max and min temperature removed

--- Frontend/src/Components/city_component.jsx
+++ Frontend/src/Components/city_component.jsx
@@ -14,12 +14,12 @@ const city_component = ({ citylist, handleremove, handleUpdate }) => {
     <div className="relative border h-36 px-4 py-2 border-zinc-200 rounded-lg w-full bg-white mt-[4px] ">
       {`${city}, ${ISO} `}
       <div className="absolute top-8 left-3  text-6xl">{Math.round(temp)}°C</div>
-      <div className="absolute font-bold text-red-500 top-9 left-36">
+      {/* <div className="absolute font-bold text-red-500 top-9 left-36">
         {Math.round(temp_max)}°C
       </div>
       <div className="absolute font-bold text-blue-500 bottom-12 left-36">
         {Math.round(temp_min)}°C
-      </div>
+      </div> */}
       <div className="absolute bottom-[4px] flex flex-col w-3/5 xl:w-1/2 text-sm h-10">
         <span>Feels Like <span className="font-semibold">
           {Math.round(feels_like)}°C
