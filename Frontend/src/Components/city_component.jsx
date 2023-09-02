40b2fb93372b86f920a321bf3024eaf347d8e8d5
Soumyadeep Ghosh <92305008+KillerMax4299@users.noreply.github.com>
2023-09-02 13:35:57 +0530

    1.1.3
    final release

--- Frontend/src/Components/city_component.jsx
+++ Frontend/src/Components/city_component.jsx
@@ -1,21 +1,34 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import useLocalStorage from "../Hooks/UseLocalStorage";
 import { MdRefresh, MdDelete } from "react-icons/md";
 
 const city_component = ({ citylist, handleremove, handleUpdate }) => {
   const { ISO, city, main, weather, time } = citylist;
-  const [count, setCount] = useLocalStorage("ct_list", []);
+  // const [count, setCount] = useLocalStorage("ct_list", []);
   const { description, icon } = weather;
-  const country = count.filter((e) => e.code === ISO);
+  // const country = count.filter((e) => e.code === ISO);
 
   const { temp, feels_like, temp_min, temp_max, humidity } = main;
 
   return (
-    <div className="relative border h-32 px-4 py-2 border-zinc-200 rounded-lg w-full bg-white mt-[4px] ">
+    <div className="relative border h-36 px-4 py-2 border-zinc-200 rounded-lg w-full bg-white mt-[4px] ">
       {`${city}, ${ISO} `}
-      <b>{Math.round(temp)}°C</b>
+      <div className="absolute top-8 left-3  text-6xl">{Math.round(temp)}°C</div>
+      <div className="absolute font-bold text-red-500 top-9 left-36">
+        {Math.round(temp_max)}°C
+      </div>
+      <div className="absolute font-bold text-blue-500 bottom-12 left-36">
+        {Math.round(temp_min)}°C
+      </div>
+      <div className="absolute bottom-[4px] flex flex-col w-3/5 xl:w-1/2 text-sm h-10">
+        <span>Feels Like <span className="font-semibold">
+          {Math.round(feels_like)}°C
+        </span>
+        </span>
+        <span> Humidity {humidity}%</span>
+      </div>
       <button
-        className=" text-2xl hover:bg-red-200 p-1 rounded-full active:opacity-80 text-zinc-400 hover:text-red-500 transition-colors duration-200"
+        className="absolute bottom-1 right-1 text-2xl hover:bg-red-200 p-1 rounded-full active:opacity-80 text-zinc-400 hover:text-red-500 transition-colors duration-200"
         onClick={() => {
           handleremove(city);
         }}
@@ -23,31 +36,22 @@ const city_component = ({ citylist, handleremove, handleUpdate }) => {
         <MdDelete />
       </button>{" "}
       <button
-        className="text-2xl hover:bg-zinc-200 p-1  rounded-full active:opacity-80 text-zinc-400 hover:text-zinc-600 transition-colors duration-200"
+        className="absolute top-2 right-1 text-2xl hover:bg-zinc-200 p-1  rounded-full active:opacity-80 text-zinc-400 hover:text-zinc-600 transition-colors duration-200"
         onClick={() => {
           handleUpdate(city);
         }}
       >
         <MdRefresh />
       </button>
-      <br />
-      Feels Like
-      <b>{" " + Math.round(feels_like)}°C</b> | Humidity {humidity}%
-      <br />
-      <span className="font-bold text-red-500">
-        {Math.round(temp_max)}°C
-      </span>{" "}
-      <span className="font-bold text-blue-500">{Math.round(temp_min)}°C</span>
-      <br />
-      <span
-        className="absolute top-0 right-3">
-        <img 
+      <span className="absolute flex flex-col justify-center items-center -top-2 right-10 text-center">
+        <p className="translate-y-4 font-bold">{time}</p>
+        <img
+          className="h-24 w-24 translate-y-2"
           src={`/icons/${icon}.png`}
           alt=""
         />
-        <p className="text-center -translate-y-3">{description }</p>
+        <p className="">{description}</p>
       </span>
-      <b>{time}</b>
     </div>
   );
 };
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Weather",
+        short_name: "Weather",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#fff",
+        lang: "en",
+        scope: "/",
+        icons: [
+          {
+            src: "/logos/maskable_icon.png",
+            sizes: "120x120",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/logos/maskable_icon_x96.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/logos/maskable_icon_x72.png",
+            sizes: "72x72",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/logos/maskable_icon_x48.png",
+            sizes: "48x48",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#a6a6a6",
+      },
+    }),
+  ],
+});
